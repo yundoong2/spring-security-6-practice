1e2524190f3ad4ffed3eb52f1df47d4c48df55ea
cyh6809 <cyh6809@gmail.com>
2023-05-13 23:06:44 +0900

    SecurityConfig 및 Controller 초기 작업

---
+++ server/login-custom-filter/src/main/java/com/sp/fc/controller/TeacherController.java
@@ -0,0 +1,16 @@
+package com.sp.fc.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/teacher")
+public class TeacherController {
+    @PreAuthorize("hasAnyAuthority('ROLE_TEACHER')")
+    @GetMapping
+    public String main() {
+        return "TeacherMain";
+    }
+}
